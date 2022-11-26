110fba590bcdd4552970496e6a0a59c53f365b91
Hyunjae Kim <hyunjae.kim@gmx.com>
2022-11-26 16:57:37 +0900

    fix: Bug fix when creating checkbox

--- lua/asciidoc-wiki/list.lua
+++ lua/asciidoc-wiki/list.lua
@@ -5,10 +5,17 @@ local var =  require('asciidoc-wiki.var')
 M.toggle_list_item = function()
   local unordered_list_regex = "[ \t]*\\*\\+[ \t]\\+"
 
+  --- @type string
   local linestr = vim.fn.getline(".")
 
   -- Check checkbox
   local _, u_end = vim.regex(unordered_list_regex):match_str(linestr)
+
+  if u_end == nil then
+    print("Not an unordered list")
+    return
+  end
+
   local checkbox = linestr:sub(u_end+1, u_end + 3)
   local checkbox_start, _ = vim.regex("\\[[ *x]\\]"):match_str(checkbox)
 
